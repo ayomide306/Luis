f53c07d37baee263346e3251b49f184bde3bb757
Bmakanju <brilliantmakanju3@gmail.com>
2021-12-03 23:36:51 +0100

    LuisBlog Hosting

---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path('ayen/', admin.site.urls),
+    path("", include("post.urls", namespace="post")),
+    path("ckeditor/", include("ckeditor_uploader.urls")),
+    path("", include("account.urls", namespace="account")),
+    path("", include("dashboard.urls", namespace="dashboard")),
+    path("", include("newsletter.urls", namespace="newsletter")),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
